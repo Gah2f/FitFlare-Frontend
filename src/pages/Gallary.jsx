fa75d74cf22c40143d1110e005bef5cca93b9f0f
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2025-03-09 18:43:04 +0300

    Gallary page changed to swiper

--- src/pages/Gallary.jsx
+++ src/pages/Gallary.jsx
@@ -1,23 +1,18 @@
 import React from "react";
-import Dumpel from "../assets/imgs/Dumpel.jpg";
-import Flexy from "../assets/imgs/Flexy.jpg";
-import Gymman from "../assets/imgs/Gymman.jpg";
-import excerise from "../assets/imgs/Inclass.jpg";
-import Swim from "../assets/imgs/Swim.jpg";
-import CarouselImg from "../components/Carusel/CarouselImg";
+import SwiperImg from "../components/SwiperImg/SwiperImg";
 
 function Gallary() {
   return (
-    <div className=" md:w-[80%] mx-auto my-28 ">
-      <div className="mb-16">
+    <div className=" ">
+      <div className="mb-16 md:w-[80%] mx-auto my-28 ">
         <h1 className="text-5xl font-bold text-center  text-black mb-8">
           Our Gallary
         </h1>
       </div>
 
     
-      <div className="justify-center pl-90">
-      <CarouselImg/>
+      <div className="flex justify-center items-center  mx-auto w-[150] h-[100]">
+      <SwiperImg/>
       </div>
     </div>
   );
