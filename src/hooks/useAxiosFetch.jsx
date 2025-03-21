7c9c1498242bde8c6141c4ec73dabd11023678b4
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2025-03-21 20:40:48 +0300

    Hook for fetching

--- src/hooks/useAxiosFetch.jsx
+++ src/hooks/useAxiosFetch.jsx
@@ -5,9 +5,6 @@ function useAxiosFetch() {
     const token = localStorage.getItem('token');
     const axiosInstance = axios.create({
         baseURL: 'http://localhost:3000/',
-        headers: {
-            Authorization: token ? `Bearer ${token}` : "" ,
-        }
       }); 
 
       useEffect(()=>{
