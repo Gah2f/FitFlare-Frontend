601548c4768c58106bdca56e57effa19398581ee
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2025-03-22 12:02:17 +0300

    Design edited

--- src/pages/Classes/EachClass.jsx
+++ src/pages/Classes/EachClass.jsx
@@ -18,24 +18,24 @@ function EachClass() {
     }, [id]); 
     // console.log(DispayEach);
   return (
-    <div className='paddingcontroller'>
+    <div className='paddingcontroller '>
         {
           DispayEach.map((detail,index)=>(
             <div key={index}>
-              <div className='flex flex-col md:flex-row '>
-              <div className='w-1/2'>
+              <div className='flex flex-col md:flex-row bg-blue-100 '>
+              <div className='w-1/2 ml-4 mt-2 '>
               <h1 className='text-5xl font-bold'>{detail?.name}</h1>
               <h3 className='text-lg '>Instructor: {detail?.instructorName}</h3>
               <button className='mt-6 text-xl text-white p-4 bg-blue-500 hover:bg-blue-600 rounded-sm'>Enroll now</button>
-              <p className='text-md '> {detail?.totalEnrolled} <span className='text-xl font-bold'>already enrolled  </span> </p>
+              <p className='text-3xl mt-8 '> {detail?.totalEnrolled} <span className='text-2xl font-bold'>already enrolled  </span> </p>
               </div>
 
               <div className='w-1/2'>
-              <img src={detail?.image} alt="Detail page image" />
+              <img src={detail?.image} alt="Detail page image" className='rounded-md shadow-2xl' />
               </div>
               </div>
 
-              <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5 divide-x divide-gray-300  rounded-2xl shadow-2xl  mt-8 mr-8 h-30'>
+              <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5 divide-x divide-gray-300  rounded-2xl shadow-2xl  mt-30 mr-8 h-30 '>
                 <div className='px-6 py-4 border-r border-gray-300 text-center'>
                   <span className='text-xl font-bold'>{detail?.availableSeats} </span>
                  <p className='text-sm text-gray-600 mt-4'>
@@ -68,11 +68,58 @@ function EachClass() {
                 </p>
                 </div>
               </div>
+              <div className='flex gap-4 mt-16'> 
+              <div className='w-1/2 border-r-2 border-blue-500'>
+                <h1 className='text-7xl text-blue-500 font-bold mt-16 text-center '>About</h1>
+                {
+                  <div>
+                    <h1 className='text-2xl text-center '>
+                      {detail?.description}
+                    </h1>
+                  </div>
+                }
+              </div>
+              <div className='w-1/2'>
+                <h1 className='text-4xl mt-16 '>What you'll learn 👀</h1>
+                {
+                  detail?.Skillsgain?.map((skill,index)=>{
+                    return(
+                      <div className='mt-8'>
+                        <h3 className='text-2xl ml-6'>
+                        ✅  {skill}
+                        </h3>
+                      </div>
+                    )
+                  })
+                }
+              </div>
+              </div>
+             
 
-
+              <div>
+                <h1 className='text-7xl text-blue-500 font-bold mt-16 text-center '>
+                  Testimonials
+                </h1>
+                <div>
+                  <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                 {
+                  detail?.Testimonials?.map((testimonial)=>{
+                    return (
+                      <div className='border-2 border-gray-500 mt-16 rounded-md '>
+                        <h1 className='text-2xl font-bold'>{testimonial?.name}</h1>
+                        <p className='mt-4 text-lg'>{testimonial?.feedback}</p>
+                      </div>
+                    )
+                  })
+                 }
+                  </div>
+                  
+                </div>
+              </div>
             </div>
           ))
-        }
+          
+        } 
     </div>
   )
 }
