bd0c7c4f1eb880fe7b58a0fb4263d557b1b1d364
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2025-03-29 13:29:46 +0300

    App wraped by AuthProvider and QueryClientProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter} from 'react-router';
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router";
+import AuthProvider from "./utilities/provider/AuthProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient(); 
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryClientProvider>
+  </BrowserRouter>
+);
