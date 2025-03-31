2a718c8b5c871fb44184610ad3e52c84c1b5de70
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2025-03-31 16:32:17 +0300

    Edited

--- src/pages/Home/Hero.jsx
+++ src/pages/Home/Hero.jsx
@@ -1,31 +1,53 @@
 import React from 'react'
+import  useAuth  from '../../hooks/useAuth.jsx';
 function Hero() {
+  const { user } = useAuth();
   return (
-    <div className='relative h-screen bg-cover bg-center pt-50' style={{backgroundImage:"url('/imgs/herofitness.jpg')" }}>
-{/* Hero texts  */}
-     <div className=' absolute inset-0 bg-black/50  flex justify-center  text-center font-mono items-center px-4 '>
-     <div>
-        <h1 className=' text-3xl text-white' >
-          We provide <br /> 
-          <span className='text-5xl text-white  font-bold'> Best Fitness Platform </span> 
-        </h1>
+    <div>
+      {
+        user? (
+          <div className='paddingcontroller'>
+            <h1 className='text-5xl font-bold text-center'>Welcome, <span className='font-semibold text-blue-500'> {user?.displayName} </span> </h1>
 
-        <p className='text-xl text-gray-400 mt-4'>
-            Join expert-led fitness classes, track your progress, and achieve your health goals with FitFlare
-        </p>
-           
-           <div className='mt-4 flex justify-center items-center space-x-4'>
-             <button className='py-2 px-6 rounded bg-blue-600  hover:bg-blue-700 text-white focus:outline-none focus:ring-2 focus:ring-blue-300'>
-               Join today
-             </button>
-             <button className='py-2 px-6 rounded  hover:bg-blue-700 border-2 border-white text-white focus:outline-none focus:ring-2 focus:ring-blue-300'>
-               View Courses
-             </button>
-           </div>
-        
-      </div>
-     </div>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 text-2xl text-gray-500 border-b-2 border-blue-300 hover:border-b-blue-500 mt-8 '>
+              <ul className='flex justify-center gap-4 cursor-pointer'>
+                <li className='text-2xl hover:text-blue-400 hover:-translate-y-1 duration-300'>My Courses</li>
+                <li className='text-2xl hover:text-blue-400 hover:-translate-y-1 duration-300'>My Progress</li>
+                <li className='text-2xl hover:text-blue-400 hover:-translate-y-1 duration-300'>Certificates</li>
+              </ul>
+            </div>
+          </div>
+        ) : (
+          <div className='relative h-screen bg-cover bg-center pt-50' style={{backgroundImage:"url('/imgs/herofitness.jpg')" }}>
+          {/* Hero texts  */}
+               <div className=' absolute inset-0 bg-black/50  flex justify-center  text-center font-mono items-center px-4 '>
+               <div>
+                  <h1 className=' text-3xl text-white' >
+                    We provide <br /> 
+                    <span className='text-5xl text-white  font-bold'> Best Fitness Platform </span> 
+                  </h1>
+          
+                  <p className='text-xl text-gray-400 mt-4'>
+                      Join expert-led fitness classes, track your progress, and achieve your health goals with FitFlare
+                  </p>
+                     
+                     <div className='mt-4 flex justify-center items-center space-x-4'>
+                       <button className='py-2 px-6 rounded bg-blue-600  hover:bg-blue-700 text-white focus:outline-none focus:ring-2 focus:ring-blue-300'>
+                         Join today
+                       </button>
+                       <button className='py-2 px-6 rounded  hover:bg-blue-700 border-2 border-white text-white focus:outline-none focus:ring-2 focus:ring-blue-300'>
+                         View Courses
+                       </button>
+                     </div>
+                  
+                </div>
+               </div>
+              </div>
+        )
+      }
+    
     </div>
+  
   )
 }
 
--- src/pages/Log/SignUp.jsx
+++ src/pages/Log/SignUp.jsx
@@ -1,141 +1,182 @@
 import React, { useState } from "react";
-import useAxiosFetch from '../../hooks/useAxiosFetch'
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import { Link } from "react-router";
-
+import useAxiosFetch from "../../hooks/useAxiosFetch";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import { Link, useNavigate } from "react-router-dom";
+import GoogleLogin from "../../components/media/GoogleLogin";
+import useAuth from "../../hooks/useAuth";
+import axios from "axios";
 function SignUp() {
+  const navigate = useNavigate();
+  const {signUp, updateUser} = useAuth();
   const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState(null);
   const axiosFetch = useAxiosFetch();
-  const [formData, setFormData]= useState({
+  const [formData, setFormData] = useState({
     FirstName: "",
     LastName: "",
     email: "",
     password: "",
     confirmPassword: "",
-    agree: false,  
-  })
-  
+    agree: false,
+  });
+
   const handleChange = (e) => {
-    const {name, value, type, checked } = e.target;
+    const { name, value, type, checked } = e.target;
     setFormData({
       ...formData,
       [name]: type === "checkbox" ? checked : value,
     });
+  };
 
-    
-  }
-
-  const handleSubmit = async (e)=> {
+  const onSubmit = async (e)=>{
     e.preventDefault();
-    if(formData.password !== formData.confirmPassword){
-      alert("Password does not match");
-      return;
-    } 
-    alert("Form submitted");
+    setError('');
+    const fullName = `${formData.FirstName} ${formData.LastName}`
 
     try {
-      const response = await axiosFetch.post('/newUser', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          FirstName: formData.FirstName,
-          LastName: formData.LastName,
-          email: formData.email,
-          password: formData.password,
+      const result = await signUp(formData.name, formData.password);
+      const user = result.user;
+      if (user) {
+        updateUser(fullName, formData.photoURL);
+
+        const userIMP = {
+          name: fullName,
+          email: user?.email,
+          photo: user?.photoURL,
+          gender: data?.gender,
+          address: data?.address,
           role: 'user',
-        })
-      })
-      // const data = response.json();
-      if(!response.ok) {
-        throw new Error(data.message || 'Something went wrong');
-      }
-    } catch (error) {
-      console.log(error);
+          phone: data?.phone,
+        };
+        await axios.post('http://localhost:3000/newUser', userIMP);
+        navigate('/')
     }
-      
+   
+
+    
+
+    
+    } catch (err) {
+      setError(err.message)
+    }
   }
+
+  const handleSubmit = async (e) => {
+    
+    setError('')
+    
+  };
   return (
     <div className="pt-32 pb-72  min-h-screen justify-center items-center flex  bg-blue-300 shadow-b-2xl">
       <div className="p-8 bg-white  rounded-2xl shadow-lg w-full max-w-md ">
-        <h2 className="text-3xl font-bold text-center text-blue-600 mb-6">Sign Up</h2>
-      <form className=" space-y-4">
-        <input  
-        type="text" 
-        name="FirstName"
-        className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 outline-none" 
-        placeholder="First Name"
-        value={formData.FirstName}
-        onChange={handleChange}
-        required />
-        <input  
-        type="text" 
-        name="LastName"
-        className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 outline-none" 
-        placeholder="Last Name"
-        value={formData.LastName}
-        onChange={handleChange}
-        required />
-        <input  
-        type="email" 
-        name="email"
-        className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 outline-none" 
-        placeholder="example @gmail.com"
-        value={formData.email}
-        onChange={handleChange}
-        required />
-        <div className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 outline-none space-x-35" >
-        <input  
-        type={showPassword ? "text" : "password"} 
-        name="password"
-        className="w-1/2 overflow-hidden outline-none"
-        placeholder="Password"
-        value={formData.password}
-        onChange={handleChange}
-        required /> <span onClick={()=>setShowPassword(!showPassword)}> <VisibilityIcon className="text-gray-400 hover:text-blue-400 cursor-pointer"/> </span> 
-        </div>
-       
-        <input  
-        type="password" 
-        name="confirmPassword"
-        className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 outline-none" 
-        placeholder="ConfirmPassword"
-        value={formData.confirmPassword}
-        onChange={handleChange}
-        required />
-        <div className="flex items-center space-x-2">
-          <input 
-          type="checkbox"
-          name="agree"
-          className="w-4 h-4"
-          value={formData.agree}
-          onChange={handleChange}
-          required />
-          <label 
-          htmlFor=""
-          className="text-gray-600">
-            I agree to the terms and conditions.
-          </label>
-        </div>
-        
-        <button 
-        className="p-3 bg-blue-500 hover:bg-blue-600 text-white rounded-lg font-semibold transition justify-center w-full mt-4"
-        
-        type="submit">
-          Create Account
-        </button>
-        
-        <p className="text-center text-gray-600 mt-4">
-          Already have an account? 
-          <Link to='/login'
-          className="pl-2 text-blue-500 hover:underline">
-            Log in
-          </Link>
-        </p>
-      </form>
+        <h2 className="text-3xl font-bold text-center text-blue-600 mb-6">
+          Sign Up
+        </h2>
+        <form onSubmit={onSubmit} className=" space-y-4">
+          <input
+            type="text"
+            name="FirstName"
+            className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 outline-none"
+            placeholder="First Name"
+            value={formData.FirstName}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="text"
+            name="LastName"
+            className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 outline-none"
+            placeholder="Last Name"
+            value={formData.LastName}
+            onChange={handleChange}
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 outline-none"
+            placeholder="example @gmail.com"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+
+          <input
+            type="text"
+            name=""
+            className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 outline-none"
+            placeholder="Enter you Profile photoURL"
+            value={formData.photoURL}
+            onChange={handleChange}
+            required
+          />
+
+          <div className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 outline-none space-x-35">
+            <input
+              type={showPassword ? "text" : "password"}
+              name="password"
+              className="w-1/2 overflow-hidden outline-none"
+              placeholder="Password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />{" "}
+            <span onClick={() => setShowPassword(!showPassword)}>
+              {" "}
+              <VisibilityIcon className="text-gray-400 hover:text-blue-400 cursor-pointer" />{" "}
+            </span>
+          </div>
+
+          <input
+            type="password"
+            name="confirmPassword"
+            className="w-full p-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 outline-none"
+            placeholder="ConfirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+            required
+          />
+          <div className="flex items-center space-x-2">
+            <input
+              type="checkbox"
+              name="agree"
+              className="w-4 h-4"
+              checked={formData.agree}
+              onChange={handleChange}
+              required
+            />
+            <label htmlFor="" className="text-gray-600">
+              I agree to the terms and conditions.
+            </label>
+          </div>
+
+          <button
+            className="p-3 bg-blue-500 hover:bg-blue-600 text-white rounded-lg font-semibold transition justify-center w-full mt-4 cursor-pointer"
+            type="submit"
+          >
+            Create Account
+          </button>
+      {
+         formData.password && formData.confirmPassword &&
+        formData.password !== formData.confirmPassword && (
+          <div>
+            <p className="text-sm text-red-500 text-center">
+              Password must match
+            </p>
+          </div>
+        )
+      }
+          <div>
+            <GoogleLogin />
+          </div>
+          <p className="text-center text-gray-600 mt-4">
+            Already have an account?
+            <Link to="/login" className="pl-2 text-blue-500 hover:underline">
+              Log in
+            </Link>
+          </p>
+        </form>
       </div>
-    
     </div>
   );
 }
